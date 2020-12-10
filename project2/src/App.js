0a90461efc219772b2bdbff56386b489d0b36d08
Guptanator-boop <KushalGupta@gmail.com>
2020-12-10 15:14:36 -0500

    Added React Wev App

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,5 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {Memory} from './Memory'
+
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const fs = require('fs');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+let connection = mysql.createConnection(credentials);
+connection.connect();
+
+function rowToObject(row) {
+  return {
+    username: row.username,
+    movie_name: row.movie_name,
+    rating: row.rating,
+    message: row.message,
+  }
+}
+
+app.get('/reviews/:movie_name', (request, response) => {
+  const query = 'SELECT username, movie_name, rating, message FROM review ORDER BY updated_at DESC';
+  const params = [request.params.movie_name];
+  connection.query(query, params, (error, rows) => {
+    response.send({
+      ok: true,
+      reviews: rows.map(rowToObject),
+    });
+  });
+});
+
+app.post('/reviews', (request, response) => {
+  const query = 'INSERT INTO review(username, movie_name, rating, message)  FROM review ORDER BY updated_at DESC';
+  const params = [request.params.movie_name];
+  connection.query(query, params, (error, rows) => {
+    response.send({
+      ok: true,
+      reviews: rows.map(rowToObject),
+    });
+  });
+});
+
+const port = 443;
+app.listen(port, () => {
+  console.log(`We live on port ${port}!`);
+});
 
 function App() {
   return (
