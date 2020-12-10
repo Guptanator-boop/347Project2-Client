dd94c867396817836916fa8e6aa00da004e5bf94
Guptanator-boop <KushalGupta@gmail.com>
2020-12-10 15:49:13 -0500

    Added React Wev App

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,20 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import {Memory} from './Memory'
+import {review} from './review'
 
+//Define constants
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 const fs = require('fs');
 
+//Set up core and express
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Get credentials to access database
 let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 let connection = mysql.createConnection(credentials);
 connection.connect();
 
+//Function to convert row to object
 function rowToObject(row) {
   return {
     username: row.username,
@@ -24,8 +28,9 @@ function rowToObject(row) {
   }
 }
 
+//Get from database
 app.get('/reviews/:movie_name', (request, response) => {
-  const query = 'SELECT username, movie_name, rating, message FROM review ORDER BY updated_at DESC';
+  const query = 'SELECT username, movie_name, rating, message FROM review ORDER BY created_at DESC';
   const params = [request.params.movie_name];
   connection.query(query, params, (error, rows) => {
     response.send({
@@ -35,13 +40,36 @@ app.get('/reviews/:movie_name', (request, response) => {
   });
 });
 
+//Add to database
 app.post('/reviews', (request, response) => {
-  const query = 'INSERT INTO review(username, movie_name, rating, message)  FROM review ORDER BY updated_at DESC';
-  const params = [request.params.movie_name];
-  connection.query(query, params, (error, rows) => {
+  const query = 'INSERT INTO review(username, movie_name, rating, message) VALUES (?, ?, ?, ?)';
+  const params = [request.body.username, request.body.movie_name, request.body.rating, request.body.message];
+  connection.query(query, params, (error, result) => {
     response.send({
       ok: true,
-      reviews: rows.map(rowToObject),
+      id: result.insertID,
+    });
+  });
+});
+
+//Update database entry
+app.patch('/reviews:id', (request, response) => {
+  const query = 'UPDATE review SET message = ?, rating = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?';
+  const params = [request.body.username, request.body.movie_name, request.body.rating, request.body.message, request.params.id];
+  connection.query(query, params, (error, result) => {
+    response.send({
+      ok: true,
+    });
+  });
+});
+
+//Remove database entry
+app.delete('/reviews:id', (request, response) => {
+  const query = 'UPDATE review SET movie_name = 0, updated_at = CURRENT_TIMESTAMP WHERE id = ?';
+  const params = [request.body.username, request.body.movie_name, request.body.rating, request.body.message, request.params.id];
+  connection.query(query, params, (error, result) => {
+    response.send({
+      ok: true,
     });
   });
 });
@@ -55,18 +83,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ project2/src/review.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function Memory(props){
+    const review = props.review;
+
+    return (
+        <div className="review">
+            <div className="review-left">
+                <span className="username">{review.username}</span>
+                <span className="movie_name">{review.movie_name}</span>
+                </div>
+            <div className="review-right">
+                <span className="rating">{review.rating}</span>
+                <span className="message">{review.message}</span>
+            </div>
+        </div>
+    );
+}
