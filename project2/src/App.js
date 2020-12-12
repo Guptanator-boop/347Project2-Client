958d09dad9ea0d12d13f173c543e21e9e004363b
Guptanator-boop <KushalGupta@gmail.com>
2020-12-11 19:45:49 -0500

    frontend part 1

--- project2/src/App.js
+++ project2/src/App.js
@@ -5,11 +5,25 @@ import {Review} from './Review'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Vinsand is a bitch
-        </p>
-      </header>
+      <div class="banner">
+        <h1>Movie2Watch.com</h1>
+      </div>
+      <div class="body">
+        <form action="/action_page.php">
+        <input type="text" placeholder="Movie name.." name="search"></input>
+        <button type="submit">Search</button>
+        </form>
+        <div class="movies-display">
+          <h2>Trending Movies</h2>
+          <div class="trending">
+          </div>
+        </div>
+        <div class="movies-display">
+          <h2>Most Watched</h2>
+          <div class="top">
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
