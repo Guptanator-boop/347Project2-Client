f5d47f4a2c1dd3542fce9e871f71d79d7b15e5ed
Guptanator-boop <KushalGupta@gmail.com>
2020-12-11 20:11:06 -0500

    ex

--- project2/src/App.js
+++ project2/src/App.js
@@ -5,25 +5,31 @@ import {Review} from './Review'
 function App() {
   return (
     <div className="App">
-      <div class="banner">
+      <div class="top">
         <h1>Movie2Watch.com</h1>
-      </div>
-      <div class="body">
         <form action="/action_page.php">
         <input type="text" placeholder="Movie name.." name="search"></input>
         <button type="submit">Search</button>
         </form>
+      </div>
+      <div class="body">
         <div class="movies-display">
           <h2>Trending Movies</h2>
           <div class="trending">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71NPmBOdq7L._AC_SL1333_.jpg"></img>
           </div>
         </div>
         <div class="movies-display">
           <h2>Most Watched</h2>
-          <div class="top">
+          <div class="top-movies">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81ai6zx6eXL._AC_SL1304_.jpg"></img>
           </div>
         </div>
       </div>
+      <footer>
+          <p>Made using <a href="https://developers.themoviedb.org/3/getting-started/introduction">The Movie Database</a></p>
+          <p>Developed by Kyle Vinsand and Kushal Gupta</p>
+        </footer>
     </div>
   );
 }
